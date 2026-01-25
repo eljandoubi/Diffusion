6eb6d1d77dfd6491c9cf4ad1877b829ad9ba98a9
ejandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-01-25 17:56:29 +0100

    SinusoidalTimeEmbedding

--- src/model.py
+++ src/model.py
@@ -51,7 +51,11 @@ class SelfAttention(nn.Module):
 
 class MLP(nn.Module):
     def __init__(
-        self, in_channels: int, mlp_ratio: int = 4, act_layer=nn.GELU, mlp_p: float = 0
+        self,
+        in_channels: int,
+        mlp_ratio: float = 4,
+        act_layer=nn.GELU,
+        mlp_p: float = 0,
     ):
         super().__init__()
         hidden_features = int(in_channels * mlp_ratio)
@@ -68,3 +72,76 @@ class MLP(nn.Module):
         x = self.fc2(x)
         x = self.drop2(x)
         return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        fused_attention: bool = True,
+        num_heads: int = 4,
+        mlp_ratio: float = 2,
+        proj_p: float = 0,
+        attn_p: float = 0,
+        mlp_p: float = 0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(in_channels, eps=1e-6)
+
+        self.attn = SelfAttention(
+            in_channels=in_channels,
+            num_heads=num_heads,
+            attn_p=attn_p,
+            proj_p=proj_p,
+            fused_attn=fused_attention,
+        )
+
+        self.norm2 = norm_layer(in_channels, eps=1e-6)
+        self.mlp = MLP(
+            in_channels=in_channels,
+            mlp_ratio=mlp_ratio,
+            act_layer=act_layer,
+            mlp_p=mlp_p,
+        )
+
+    def forward(self, x: torch.Tensor):
+        batch_size, channels, height, width = x.shape
+
+        ### Reshape to batch_size x (height*width) x channels
+        x = x.reshape(batch_size, channels, height * width).permute(0, 2, 1)
+
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+
+        x = x.permute(0, 2, 1).reshape(batch_size, channels, height, width)
+        return x
+
+
+class SinusoidalTimeEmbedding(nn.Module):
+    def __init__(self, time_embed_dim, scaled_time_embed_dim):
+        super().__init__()
+        self.inv_freqs = self.register_buffer(
+            "inv_freqs",
+            1.0
+            / (
+                10000
+                ** (torch.arange(0, time_embed_dim, 2).float() / (time_embed_dim / 2))
+            ),
+        )
+
+        self.time_mlp = nn.Sequential(
+            nn.Linear(time_embed_dim, scaled_time_embed_dim),
+            nn.SiLU(),
+            nn.Linear(scaled_time_embed_dim, scaled_time_embed_dim),
+            nn.SiLU(),
+        )
+
+    def forward(self, timesteps: torch.Tensor):
+        timestep_freqs = timesteps.unsqueeze(1) * self.inv_freqs.unsqueeze(0)
+        embeddings = torch.cat(
+            [torch.sin(timestep_freqs), torch.cos(timestep_freqs)], axis=-1
+        )
+        embeddings = self.time_mlp(embeddings)
+        return embeddings
